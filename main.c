2f73f4b51e4f392aedf2661136501e9d7277ab1c
yelim7316 <70632863+yelim7316@users.noreply.github.com>
2020-10-08 16:43:07 +0900

    04

--- main.c
+++ main.c
@@ -3,22 +3,52 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-
-int square(int a)
-{   
-    
-   return (a*a);
-
+int factorial(int n) 
+{
+	int i, res=1;
+	
+	for(i=1;i<=n;i++) {
+		res = res * i;
+	}
+	return res;
 }
 
+int combination(int n, int r)
+{   int n1, n2, n3;
+  n1= factorial(n);
+  n2 = factorial(n-r);
+  n3 = factorial(r);
+  
+  return (n1/n2/n3);
+   
+}
+
+
+
+int get_char()
+{
+	int x;
+	printf("input an integer: ");
+	scanf("%i", &x);
 	
+	return x;
+	}	
+
 int main(int argc, char *argv[])
 {	
-	int a =2;
+	//변수 선언
+	int n,r, result;
 	
-	a = square(a);
-	printf("a=%i\n",a);
+	//n 입력 
+	n = get_char();
+	//r  입력
+	r = get_char();
+	 
+	//combinaion
+	result = combination(n,r);
 	
+	printf("combination result is %i\n", result);
+	 
 	return 0;
 }		    
 
